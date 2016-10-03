4f79189a60bdf67cfa02197f3b33eb0c9eda1f9b
ekilfeather <ekilfeather@gmail.com>
2016-10-03 17:44:35 +0100

    ssl on host

--- lib/omniauth/strategies/orcid.rb
+++ lib/omniauth/strategies/orcid.rb
@@ -10,7 +10,7 @@ module OmniAuth
 
   
         option :client_options, {
-          :sites => 'http://sandbox.orcid.org',
+          :sites => 'https://sandbox.orcid.org',
           :authorize_url => 'https://sandbox.orcid.org/oauth/authorize',
           :token_url => 'https://sandbox.orcid.org/oauth/token',
           :scope => '/authorise',
